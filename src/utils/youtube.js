94927f70715f2b9512d76d2bd8e45cc6115725ad
Danny Vu <danny.hvu@gmail.com>
2019-06-04 22:16:27 +0200

    Use env var YT api

--- src/utils/youtube.js
+++ src/utils/youtube.js
@@ -4,6 +4,6 @@ module.exports = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    key: 'AIzaSyD9TZEQX9cK7QO1vcJlZxUPvJTkdi-XUwU',
+    key: process.env.YOUTUBE_API,
   },
 });
