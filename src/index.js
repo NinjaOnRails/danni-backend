84f04a0832679af2468b4199233ca9ac9d2949ea
Danny Vu <danny.hvu@gmail.com>
2019-06-11 14:46:51 +0200

    Add cors origin url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_TEST],
+      origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.FRONTEND_URL_TEST],
     },
   },
   deets => {
