a3d722f972479e20b221bef199da1db850305002
Danny Vu <danny.hvu@gmail.com>
2019-07-21 16:25:13 +0200

    Setup for now deployment

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
+  typeDefs: __dirname + "/schema_prep.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: false,
