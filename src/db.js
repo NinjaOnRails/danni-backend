fc280dd0383fd7fad87da38076fe8ffd14c40b93
Danny Vu <danny.hvu@gmail.com>
2019-07-25 12:00:08 +0200

    Change env var name

--- src/db.js
+++ src/db.js
@@ -4,8 +4,7 @@ const { Prisma } = require('prisma-binding');
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   // typeDefs: __dirname + "/schema_prep.graphql",
-  // endpoint: process.env.PRISMA_ENDPOINT_DEV,
-  endpoint: process.env.PRISMA_ENDPOINT_PROD,
+  endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: false,
 });
