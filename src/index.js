6e67fed41a46a8444da5272b28ef455d03027bae
Danny Vu <danny.hvu@gmail.com>
2019-07-26 00:32:27 +0200

    Send get request every 30min to prisma server and frontend

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
+const http = require("http");
 const db = require('./db');
 
 const server = createServer();
+
+setInterval(function() {
+    http.get("http://danni-prod-5e73c28066.herokuapp.com");
+    http.get(process.env.FRONTEND_URL);
+}, 1800000); // every 30 minutes
 
 server.start(
   {
