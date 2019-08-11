4c5c731717426ce69e8232b875ac0f6cfb102463
Danny Vu <danny.hvu@gmail.com>
2019-08-11 14:02:56 +0200

    Inspect received token

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ server.express.use(cookieParser());
 
 // Decode JWT to get User ID on each request
 server.express.use((req, res, next) => {
+  console.log(req.cookies);
   const { token } = req.cookies;
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
@@ -33,7 +34,11 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2, process.env.FRONTEND_URL_3],
+      origin: [
+        process.env.FRONTEND_URL,
+        process.env.FRONTEND_URL_2,
+        process.env.FRONTEND_URL_3,
+      ],
     },
   },
   deets => {
