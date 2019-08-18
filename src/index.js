d8f7b880c71203dccb4c609444f2e4954061e5c3
Danny Vu <danny.hvu@gmail.com>
2019-08-18 10:02:42 +0200

    Use fetch every 15min instead

--- src/index.js
+++ src/index.js
@@ -29,12 +29,6 @@ server.express.use(async (req, res, next) => {
   next();
 });
 
-// Ping itself every 30min to stay awake on Heroku
-var http = require('http');
-setInterval(function() {
-  http.get('https://dannitv.herokuapp.com').then(res => console.log(res));
-}, 300000);
-
 server.start(
   {
     cors: {
@@ -50,3 +44,8 @@ server.start(
     console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
+
+// Ping itself every 15min to stay awake on Heroku
+setInterval(() => {
+  fetch('https://dannitv.herokuapp.com');
+}, 900000);
