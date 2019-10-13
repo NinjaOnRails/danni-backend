da75a845439e247278e56d1ed2d73866de4cfa18
Danny Vu <danny.hvu@gmail.com>
2019-10-13 22:47:16 +0200

    Build cloudinary auth signature creation for avatar image profile picture upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,19 +1,37 @@
 const cloudinary = require('cloudinary').v2;
 
-module.exports = (source, userId, language) => {
-  const timestamp = new Date().getTime();
-  const params_to_sign = {
-    public_id: `${source}_${userId}_${language}`,
-    tags: `${userId},${source},${timestamp},${language}`,
-    timestamp,
-    upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET_AUDIO,
-  };
+const timestamp = new Date().getTime();
 
-  return {
-    signature: cloudinary.utils.api_sign_request(
-      params_to_sign,
-      process.env.CLOUDINARY_API_SECRET
-    ),
-    timestamp,
-  };
+module.exports = {
+  audioSign: (source, userId, language) => {
+    const params_to_sign = {
+      public_id: `${source}_${userId}_${language}`,
+      tags: `${userId},${source},${timestamp},${language}`,
+      timestamp,
+      upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET_AUDIO,
+    };
+
+    return {
+      signature: cloudinary.utils.api_sign_request(
+        params_to_sign,
+        process.env.CLOUDINARY_API_SECRET
+      ),
+      timestamp,
+    };
+  },
+  avatarSign: userId => {
+    const params_to_sign = {
+      public_id: `avatar-${userId}`,
+      tags: `${userId},${timestamp},user-avatar,user,avatar,profile-picture,picture,image,profile`,
+      timestamp,
+      upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET_AVATAR,
+    };
+    return {
+      signature: cloudinary.utils.api_sign_request(
+        params_to_sign,
+        process.env.CLOUDINARY_API_SECRET
+      ),
+      timestamp,
+    };
+  },
 };
