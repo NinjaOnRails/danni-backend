455180557955b0f12240964ed24835b1e0cf6356
Danny Vu <danny.hvu@gmail.com>
2019-11-01 20:49:39 +0100

    Add youtube mobile url

--- src/utils/extractYoutubeId.js
+++ src/utils/extractYoutubeId.js
@@ -5,17 +5,29 @@ module.exports = source => {
   source = source.trim();
   const sourceYouTube = [
     { domain: 'https://youtube.com/watch?v=', length: 28 },
+    { domain: 'https://m.youtube.com/watch?v=', length: 30 },
     { domain: 'http://youtube.com/watch?v=', length: 27 },
+    { domain: 'http://m.youtube.com/watch?v=', length: 29 },
     { domain: 'https://www.youtube.com/watch?v=', length: 32 },
+    { domain: 'https://www.m.youtube.com/watch?v=', length: 34 },
     { domain: 'http://www.youtube.com/watch?v=', length: 31 },
+    { domain: 'http://www.m.youtube.com/watch?v=', length: 33 },
     { domain: 'youtube.com/watch?v=', length: 20 },
+    { domain: 'm.youtube.com/watch?v=', length: 22 },
     { domain: 'www.youtube.com/watch?v=', length: 24 },
+    { domain: 'www.m.youtube.com/watch?v=', length: 26 },
     { domain: 'https://youtu.be/', length: 17 },
+    { domain: 'https://m.youtu.be/', length: 19 },
     { domain: 'https://www.youtu.be/', length: 21 },
+    { domain: 'https://www.m.youtu.be/', length: 23 },
     { domain: 'http://youtu.be/', length: 16 },
+    { domain: 'http://m.youtu.be/', length: 18 },
     { domain: 'http://www.youtu.be/', length: 20 },
+    { domain: 'http://www.m.youtu.be/', length: 22 },
     { domain: 'youtu.be/', length: 9 },
+    { domain: 'm.youtu.be/', length: 11 },
     { domain: 'www.youtu.be/', length: 13 },
+    { domain: 'www.m.youtu.be/', length: 15 },
   ];
   const isYouTube = sourceYouTube.find(value =>
     source.startsWith(value.domain)
